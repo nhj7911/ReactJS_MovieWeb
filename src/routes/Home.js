e887450f33a4cba55f4181c2e7326c59a600e306
Hye-Ji Na <nhj7911@gmail.com>
2022-03-09 05:44:54 +0900

    home 화면 최종

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
-import MovieDetail from "../render/MovieDetail";
+import styles from "./Home.module.css";
 import React from "react";
-import Load from "../components/Load";
-import { Group_obj, Group_key_arr } from "../NavList";
 import { Link } from "react-router-dom";
 import Slide from "../components/Slide";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+import { Group_obj, Group_key_arr } from "../NavList";
 
 function Home() {
   return (
@@ -12,9 +12,23 @@ function Home() {
       {Group_key_arr.map((group) => {
         return (
           <div key={group}>
-            <div>
-              <div>
-                <Link to={`/page/${Group_obj[group]}/1`}>{group}</Link>
+            <div className={styles.title}>
+              <div className={styles.titleBox}>
+                <Link
+                  to={`/page/${Group_obj[group]}/1`}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignContent: "center",
+                  }}
+                >
+                  <div className={styles.titleImg}>
+                    <FontAwesomeIcon icon={faCompactDisc}></FontAwesomeIcon>
+                  </div>
+                  <div>
+                    <span>{group}</span>
+                  </div>
+                </Link>
               </div>
             </div>
             <Slide
@@ -23,6 +37,13 @@ function Home() {
           </div>
         );
       })}
+      <div className={styles.footer}>
+        <div className={styles.copyright}>
+          <h3 className={styles.copyright_letter}>
+            Copyright &copy; 2021 nhj7911. All Right Reserved.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
